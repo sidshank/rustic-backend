3de1f71641d6fe8b94e5469bfc4cd101078498c3
Sid Shankar <siddharth.shankar@gmail.com>
2020-01-03 15:49:23 -0500

    Clean up variable use around closures and switch to use iterators, map and filter (#3)

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,6 @@ extern crate serde;
 
 use std::io::{BufRead, Cursor};
 use std::str::from_utf8;
-use std::cell::RefCell;
 
 use dotenv::dotenv;
 use multipart::server::Multipart;
@@ -18,21 +17,16 @@ use rocket_cors::{AllowedHeaders, AllowedOrigins, Error};
 use rocket::Data;
 use rocket::http::{ContentType, Method, RawStr, Status};
 use rocket::response::status::Custom;
-use serde::Serialize;
+use rusoto_s3::Tag;
 
 mod s3_interface;
 
-#[derive(Serialize)]
-struct S3Object {
-    file_name: String,
-    presigned_url: String,
-    tags: String,
-    e_tag: String, // AWS generated MD5 checksum hash for object
-}
+use s3_interface::{BucketContents, S3Object};
 
-#[derive(Serialize)]
-struct BucketContents {
-    data: Vec<S3Object>,
+#[derive(Debug)]
+enum S3ObjectError {
+    FileWithNoName,
+    MultipleTagsWithSameName,
 }
 
 #[get("/contents?<filter>")]
@@ -40,76 +34,91 @@ fn get_bucket_contents(filter: Option<&RawStr>) -> Result<Json<BucketContents>, 
     
     let s3_file_manager = s3_interface::S3FileManager::new(None, None, None, None);
     let bucket_contents_maybe = s3_file_manager.get_bucket_contents();
-    
     if let None = bucket_contents_maybe {
-        return Ok(Json(BucketContents {
-            data: Vec::new(),
-        }));
+        return Ok(Json(BucketContents::empty_bucket()));
     }
-
     let bucket_list = bucket_contents_maybe.unwrap();
+    let search_string = filter.unwrap_or_else(|| "".into()).as_str();
+    let should_search = search_string.len() > 0;
 
-    let mut objects_in_bucket:Vec<S3Object> = Vec::new();
-    for bucket_obj in bucket_list {
-        
-        // Skip folder names, we only care about files
-        if bucket_obj.size.unwrap_or(0) == 0 {continue}
-        
-        if let None = bucket_obj.key {
-            return Err(Custom(
-                Status::InternalServerError,
-                "Encountered bucket objects with no name".into()
-            ))
-        }
+    let bucket_contents: Result<Vec<S3Object>, S3ObjectError> = bucket_list
+        .into_iter()
+        .filter(|bucket_obj| bucket_obj.size.unwrap_or(0) != 0) // Eliminate folders
+        .map(|bucket_obj| {
+            if let None = bucket_obj.key {
+                return Err(S3ObjectError::FileWithNoName);
+            }
 
-        let file_name = bucket_obj.key.unwrap();
-        let tag_req_output = s3_file_manager.get_tags_on_file(file_name.clone());
+            let file_name = bucket_obj.key.unwrap();
+            let e_tag = bucket_obj.e_tag.unwrap_or(String::new());
+            let tag_req_output = s3_file_manager.get_tags_on_file(file_name.clone());
+            let tags_with_categories: Vec<Tag> = tag_req_output.into_iter()
+                                                            .filter(|tag| tag.key == "tags")
+                                                            .collect();
+            if tags_with_categories.len() > 1 {
+                return Err(S3ObjectError::MultipleTagsWithSameName);
+            }
 
-        let tags_with_categories: Vec<rusoto_s3::Tag> = tag_req_output.into_iter()
-                                                                      .filter(|tag| tag.key == "tags")
-                                                                      .collect();
-        if tags_with_categories.len() > 1 {
-            return Err(Custom(
-                Status::InternalServerError,
-                "Encountered a file with a more than one tag named 'tags'".into()
-            ))
-        }
+            let tag_value = if tags_with_categories.len() == 0 {
+                "".to_string()
+            } else {
+                tags_with_categories[0].value.clone()
+            };
 
-        // let tag_value = tags_with_categories[0].value.clone();
-        let tag_value = if tags_with_categories.len() == 0 {
-            "".to_string()
-        } else {
-            tags_with_categories[0].value.clone()
-        };
+            if should_search &&
+                !tag_value.contains(search_string) &&
+                !file_name.contains(search_string) {
+                Ok(S3Object::new(
+                    file_name,
+                    e_tag,
+                    tag_value,
+                    String::new(),
+                    true,
+                ))
+            } else {
+                // TODO: We don't have to go to S3 everytime we need a pre-signed link
+                // Pre-signed links should probably be stored in a cache with a reasonable
+                // expiry and fetched from the cache whenever needed.
+                let presigned_url = s3_file_manager.get_presigned_url_for_file(
+                    file_name.clone()
+                );
+                Ok(S3Object::new(
+                    file_name,
+                    e_tag,
+                    tag_value,
+                    presigned_url,
+                    false,
+                ))
+            }
+        })
+        .collect();
 
-        let search_string = filter.unwrap_or_else(|| "".into()).as_str();
-        if search_string.len() != 0 {
-            if !tag_value.contains(search_string) && !file_name.contains(search_string) {
-                // We have a non-empty search string, and it wasn't contained in the
-                // string of categories AND the filename. Skip this object and move on
-                // to the next one.
-                continue;
+        match bucket_contents {
+            Err(why) => match why {
+                S3ObjectError::FileWithNoName => Err(Custom(
+                    Status::InternalServerError,
+                    "Encountered bucket objects with no name".into()
+                )),
+                S3ObjectError::MultipleTagsWithSameName => Err(Custom(
+                    Status::InternalServerError,
+                    "Encountered a file with a more than one tag named 'tags'".into()
+                ))
+            },
+            Ok(s3_objects) => {
+                let visible_s3_objects: Vec<S3Object> = s3_objects.into_iter()
+                                                                  .filter(|obj| !obj.is_hidden())
+                                                                  .collect();
+                Ok(Json(BucketContents::new(visible_s3_objects)))
             }
         }
-
-        let presigned_url = s3_file_manager.get_presigned_url_for_file(file_name.clone());
-
-        let single_object = S3Object {
-            file_name: file_name.clone(),
-            presigned_url: presigned_url.to_owned(),
-            tags: tag_value.to_owned(),
-            e_tag: bucket_obj.e_tag.unwrap()
-        };
-        objects_in_bucket.push(single_object);
-    }
-
-    return Ok(Json(BucketContents {
-        data: objects_in_bucket,
-    }));
 }
 
 #[post("/upload", data = "<data>")]
-// signature requires the request to have a `Content-Type`
+// signature requires the request to have a `Content-Type`. The preferred way to handle the incoming
+// data would have been to use the FromForm trait as described here: https://rocket.rs/v0.4/guide/requests/#forms
+// Unfortunately, file uploads are not supported through that mechanism since a file upload is performed as a
+// multipart upload, and Rocket does not currently (As of v0.4) support this. 
+// https://github.com/SergioBenitez/Rocket/issues/106
 fn upload_file(cont_type: &ContentType, data: Data) -> Result<Custom<String>, Custom<String>> {
     // this and the next check can be implemented as a request guard but it seems like just
     // more boilerplate than necessary
@@ -120,47 +129,43 @@ fn upload_file(cont_type: &ContentType, data: Data) -> Result<Custom<String>, Cu
         ));
     }
 
-    let (_, boundary) = cont_type.params().find(|&(k, _)| k == "boundary").ok_or_else(
-        | | Custom(
+    let (_, boundary) = cont_type.params()
+                                 .find(|&(k, _)| k == "boundary")
+                                 .ok_or_else(
+        || Custom(
             Status::BadRequest,
             "`Content-Type: multipart/form-data` boundary param not provided".into()
         )
     )?;
 
-    let mut d = Vec::new();
-    data.stream_to(&mut d).expect("Unable to read");
-
     // The hot mess that ensues is some weird combination of the two links that follow
     // and a LOT of hackery to move data between closures.
     // https://github.com/SergioBenitez/Rocket/issues/106
     // https://github.com/abonander/multipart/blob/master/examples/rocket.rs
+    let mut d = Vec::new();
+    data.stream_to(&mut d).expect("Unable to read");
     let mut mp = Multipart::with_body(Cursor::new(d), boundary);
-    let file_name_outer = RefCell::new(String::new());
-    let tags_outer = RefCell::new(String::new());
-    let data_outer = RefCell::new(Vec::<u8>::new());
+
+    let mut file_name = String::new();
+    let mut categories_string = String::new();
+    let mut raw_file_data = Vec::new();
 
     mp.foreach_entry(|mut entry| {
         if *entry.headers.name == *"fileName" { 
             let file_name_vec = entry.data.fill_buf().unwrap().to_owned();
-            let file_name_inner = from_utf8(&file_name_vec).unwrap();
-            *file_name_outer.borrow_mut() = file_name_inner.to_string();
+            file_name = from_utf8(&file_name_vec).unwrap().to_string()
         } else if *entry.headers.name == *"tags" {
             let tags_vec = entry.data.fill_buf().unwrap().to_owned();
-            let tags = from_utf8(&tags_vec).unwrap();
-            *tags_outer.borrow_mut() = tags.to_string();
+            categories_string = from_utf8(&tags_vec).unwrap().to_string();
         } else if *entry.headers.name == *"file" {
-            let file_data_vec = entry.data.fill_buf().unwrap().to_owned();
-            *data_outer.borrow_mut() = file_data_vec;
+            raw_file_data = entry.data.fill_buf().unwrap().to_owned()
         }
     }).expect("Unable to iterate");
 
-    let file_name = file_name_outer.into_inner();
     let s3_file_manager = s3_interface::S3FileManager::new(None, None, None, None);
-    
-    s3_file_manager.put_file_in_bucket(file_name.clone(), data_outer.into_inner());
+    s3_file_manager.put_file_in_bucket(file_name.clone(), raw_file_data);
 
-    let concatenated_tag_string = tags_outer.into_inner();
-    let tag_name_val_pairs = vec![("tags".to_string(), concatenated_tag_string)];
+    let tag_name_val_pairs = vec![("tags".to_string(), categories_string)];
     s3_file_manager.put_tags_on_file(file_name, tag_name_val_pairs);
 
     return Ok(
